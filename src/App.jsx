e32caf2ea8d2640f8357ba8220e7a1331cc2289c
Sebas C <Sebascontreras319@gmail.com>
2025-10-30 18:56:06 -0300

    Inicio de SwipeFlix

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-red-600">SwipeFlix</h1>
+    </div>
+  );
+}
+
+export default App;
+
